0db344272879272d97e82e28a5d6e496e09a7c3a
Daniel Menezes <danielmenezes42@gmail.com>
2020-05-09 11:59:42 -0300

    escreve little endian mesmo em maquinas b.e

--- conv_utf.c
+++ conv_utf.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
-//#include "conv_utf.h"
+#include "conv_utf.h"
 
-const int BOM = 0xfeff;
+int toLittleEndian32(int input) {
+    /* retorna a versão little endian de input */
+    short num = 0xff00; 
+    unsigned char *temp = (unsigned char *)&num;
+    if (*temp == 0xff) {
+        /* maquina é big endian */
+        input = ( (input & 0xff000000u) >> 3*8 ) |
+                ( (input & 0x000000ff)  << 3*8 ) | 
+                ( (input & 0x00ff0000)  >> 8   ) |
+                ( (input & 0x0000ff00)  << 8   )
+        ;
+    }
+    return input;
+}
+
 int utf8_32(FILE *arq_entrada, FILE *arq_saida) {
-    int nBytes, c, p;
+    int i, utfchar, p;
+    const int BOM = toLittleEndian32(0xfeff);
 
     fwrite(&BOM, 4, 1, arq_saida);
-    while( (p = getc(arq_entrada)) != EOF ) {
+    while( (p = fgetc(arq_entrada)) != EOF ) {
         if (!(p & 0x80)) {
-            c = p;
+            utfchar = p;
         } else{
-            for (nBytes=2; p & ( 0x1<<(7-nBytes) ); nBytes++)
+            for (i=2; p & ( 0x1<<(7-i) ); i++)
                 ;
-            c = (int) ( p & ( 0xffu >> (nBytes+1) ) );
-            nBytes--;
-            for(; nBytes; nBytes--){
-                p = fgetc(arq_entrada);
-                c = ( c<<6 ) + ( p & 0x3f );
+            utfchar = (int) ( p & ( 0xffu >> (i+1) ) );
+            i--;
+            for(; i; i--){
+                p = fgetc(arq_entrada); if (p == EOF) break;
+                utfchar = ( utfchar<<6 ) + ( p & 0x3f );
             }
         }
-        printf("p: %x - c:%x\n", p, c);
-        fwrite(&c, 4, 1, arq_saida);
+        utfchar = toLittleEndian32(utfchar);
+        if (!fwrite(&utfchar, 4, 1, arq_saida)){
+            fprintf(stderr, "erro de gravação\n");
+            return -1;
+        }
+    }
+    if (ferror(arq_entrada)) {
+        fprintf(stderr, "erro de leitura\n");
+        return -1;
     }
     return 0;
 }
 
 int utf32_8(FILE *arq_entrada, FILE *arq_saida) {
 
-}
-
-
-int main(void) {
-    FILE *input = fopen("utf8_peq.txt", "rb");
-    FILE *output = fopen("8to32.txt", "wb");
-    utf8_32(input, output);
-    fclose(input);
-    fclose(output);
-
-    return 0;
 }
